43d3cd3ffee97b55affa15e4102fa5d1a4a41ff1
Lou <antilou86@gmail.com>
2019-06-21 13:32:03 -0400

    Stretch- working on carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,37 @@
 class Carousel {
-
+    constructor(element){
+        this.element = element;
+        this.leftButton = this.element.querySelector('.left-button');
+        this.rightButton = this.element.querySelector('.right-button');
+        this.imgData = this.element.dataset.num;
+        this.currentIndex = this.element.querySelectorAll(`img[data-num=${this.imgData}`);
+        this.indexMaxLength = this.element.querySelectorAll('img')[this.element.querySelectorAll('img').length - 1];
+        this.leftButton.addEventListener('click', () => {this.leftClick()});
+        this.rightButton.addEventListener('click', () => {this.rightClick()});       
+    }
+    const rightClick = () => {
+        let images = document.querySelectorAll('.carousel img').forEach(image => {image.style.display='none'});
+        if (this.currentIndex == this.indexMaxLength) {
+            this.currentIndex = this.element.querySelectorAll(`.img[data-num="1"`);
+            this.element.style.display= 'flex';
+        } else {
+            this.currentIndex = this.element.querySelectorAll(`.img[data-num=${this.imgData + 1}`);
+            this.element.style.display= 'flex';
+        }
+    };
+    const leftClick = () => {
+        let images = document.querySelectorAll('.carousel img').forEach(image => {image.style.display='none'});
+        if (this.currentIndex == this.element.querySelectorAll(`.img[data-num="1"`)) {
+            this.currentIndex = this.indexMaxLength;
+            this.currentIndex.style.display= 'flex';
+        } else {
+            this.currentIndex = this.element.querySelectorAll(`.img[data-num=${this.imgData - 1}`);
+            this.currentIndex.style.display= 'flex';
+        }
+    };
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel');
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
